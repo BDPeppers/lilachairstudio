0c2d18bb1604c48e205ffa08c55872cae8b7d8a2
BDPeppers <peppersbre@yahoo.com>
2020-03-09 20:08:23 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lilachairstudio/precache-manifest.38f2d86af65477d6c240b1dc56785e55.js"
+  "/lilachairstudio/precache-manifest.5e0eb34ff0d1ee5aeea10902faabddcd.js"
 );
 
 self.addEventListener('message', (event) => {
